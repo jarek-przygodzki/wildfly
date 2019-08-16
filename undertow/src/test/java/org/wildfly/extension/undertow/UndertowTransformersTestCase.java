a0113e24d46e1a6b4103f5ead49f2e77c24a442f
mnoworzyn <mnoworzyn@psi.pl>
2019-08-16 10:37:44 +0200

    [COM_WILDFLY-170] update undertow version in tests regarding changes in wildfly-core

--- undertow/src/test/java/org/wildfly/extension/undertow/UndertowTransformersTestCase.java
+++ undertow/src/test/java/org/wildfly/extension/undertow/UndertowTransformersTestCase.java
@@ -281,8 +281,7 @@ public class UndertowTransformersTestCase extends AbstractSubsystemTest {
         if (controllerVersion == ModelTestControllerVersion.EAP_7_1_0) {
             init.addMavenResourceURL(controllerVersion.getMavenGroupId() + ":wildfly-clustering-common:" + controllerVersion.getMavenGavVersion());
             init.addMavenResourceURL(controllerVersion.getMavenGroupId() + ":wildfly-web-common:" + controllerVersion.getMavenGavVersion());
-            init.addMavenResourceURL("io.undertow:undertow-servlet:2.0.4.Final");
-            init.addMavenResourceURL("io.undertow:undertow-core:2.0.4.Final");
-        }
+            init.addMavenResourceURL("io.undertow:undertow-servlet:2.0.22.Final");
+            init.addMavenResourceURL("io.undertow:undertow-core:2.0.22.Final");        }
     }
 }
